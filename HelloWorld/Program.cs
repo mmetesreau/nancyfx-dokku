e0d351611f691a8d867b0dc43fe5da3d250418dc
Mickael Metesreau <mickael.metesreau@gmail.com>
2014-07-24 19:03:00 +0200

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using System.Threading;
+using System.Linq;
+using System.Configuration;
+
+namespace HelloWorld
+{
+	class MainClass
+	{
+		private const string HOST_KEY = "host";
+
+		public static void Main (string[] args)
+		{
+			var uri = ConfigurationManager.AppSettings [HOST_KEY];
+
+			string port = "5000";
+			if (args.Length > 0) {
+				int tmp;
+				if (int.TryParse (args [0], out tmp)) {
+					port = tmp.ToString ();
+				}
+			}
+
+			var host = new NancyHost (new Uri (uri + ":" + port), new Uri("http://0.0.0.0:" + port));
+
+			host.Start (); 
+
+			Console.WriteLine(String.Format("Listening at {0}:{1}",uri,port));
+
+			while(true)
+			{
+				Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: mono HelloWorld.exe $PORT
